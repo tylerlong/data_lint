102e15f7d1e15e25e8b40a5a5730fbf9557cd9dd
Tyler Long <tyler4long@gmail.com>
2012-04-24 03:14:32 -0700

    First public version

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -1,15 +1,17 @@
+require 'pp'
+
 class SnippetsController < ApplicationController
 
   def create
     @snippet = Snippet.new(params[:snippet])
     begin
       @result = YAML.load(@snippet.content)
-    rescue
-      flash.now[:error] = 'the YAML data is invalid.'
-      render 'static_pages/home'
-      return
+    rescue => ex
+      flash.now[:error] = ex.message
+    else
+      @result = PP.pp(@result, "")
+      flash.now[:success] = 'the YAML data is valid.'
     end
-    flash.now[:success] = 'the YAML data is valid.'
     render 'static_pages/home'
   end
 
