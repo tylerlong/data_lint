336738d39f7c1b993b446c2738dd8da5c3ca9b6e
Tyler Long <tyler4long@gmail.com>
2012-10-11 10:54:42 +0800

    Runnable again

--- app/controllers/snippets_controller.rb
+++ app/controllers/snippets_controller.rb
@@ -4,35 +4,40 @@ require 'json'
 
 class SnippetsController < ApplicationController
 
-  def create
-    @snippet, @snippet1, @snippet2 = Snippet.new(params[:snippet]),
-      Snippet.new(format: 'yaml'), Snippet.new(format: 'json')
+  def index
+    @snippet = @snippet1 = @snippet2 = Snippet.new
+  end
+
+  def validate
+    @snippet, @snippet1, @snippet2 = Snippet.new, Snippet.new, Snippet.new
+    @snippet.format = params[:data_format]
+    @snippet.content = params[:data_content]
+    @snippet1.format = 'yaml'
+    @snippet2.format = 'json'
 
     if @snippet.format == 'yaml'
       @snippet1 = @snippet
-      return render 'static_pages/home' unless @snippet1.valid?
       begin
         result = YAML.load(@snippet.content)
       rescue => ex
         flash.now[:error] = ex.message
-        return render 'static_pages/home'
+        return render 'index'
       end
       @snippet2.content = JSON.pretty_generate(result)
       flash.now[:success] = 'the YAML data is valid.'
     else
       @snippet2 = @snippet
-      return render 'static_pages/home' unless @snippet2.valid?
       begin
         result = JSON.load(@snippet.content)
       rescue => ex
         flash.now[:error] = ex.message
-        return render 'static_pages/home'
+        return render 'index'
       end
       @snippet1.content = YAML.dump(result)
       flash.now[:success] = 'the JSON data is valid.'
     end
 
-    render 'static_pages/home'
+    render 'index'
   end
 
 end
--- app/controllers/static_pages_controller.rb
+++
@@ -1,7 +0,0 @@
-class StaticPagesController < ApplicationController
-
-  def home
-    @snippet = @snippet1 = @snippet2 = Snippet.new
-  end
-
-end
